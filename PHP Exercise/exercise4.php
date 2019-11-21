b04c963638652fbb77e693542a0d86dc48dc891f
Mohibur Rahman <49939788+Mohibur-CoU-ICT@users.noreply.github.com>
2019-11-21 14:43:19 +0600

    Update exercise4.php

--- PHP Exercise/exercise4.php
+++ PHP Exercise/exercise4.php
@@ -14,11 +14,14 @@
 	$name = null;
 	var_dump($name);
 	echo "<br/>";
-	
+
+	/*
 	unset($name);
 	$name = $age;
 	var_dump($name);
 	echo "<br/>";
+	*/
+
 	/*Var_dump() gives the type, size and value of the variable, 
 	whereas print_r() gives only the value.
 	Use the unset($var_name) function to get a value of NULL in the final line.*/
