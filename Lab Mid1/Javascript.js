7dec4c126abb97b57f30be1b00eae43a51aa6643
Mohibur Rahman <49939788+Mohibur-CoU-ICT@users.noreply.github.com>
2020-02-17 02:33:19 +0600

    Add files via upload

---
+++ Lab Mid1/Javascript.js
@@ -0,0 +1,11 @@
+function sayhello(){
+	// alert("Hello World!");
+	alert(this.name + " is checked!");
+	/* if($("Huey").checked){
+		document.write("Huey is checked");
+	} else if($("Dewey").checked){
+		document.write("Dewey is checked");
+	} else {
+		document.write("Louie is checked");
+	} */
+}
